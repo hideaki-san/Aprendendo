994f616fb68dde7d3a1dfa2a74faf585fedec4e7
ViniAman01 <vinisaman@gmail.com>
2023-06-29 11:31:29 -0400

    Corrigindo search.py

--- search.py
+++ search.py
@@ -9,25 +9,14 @@ driver.implicitly_wait(0.5)
 driver.get("https://en.wikipedia.org/wiki/Lists_of_schools_in_the_United_States")
 time.sleep(0.5)
 # driver.find_element(By.XPATH,f'//*[@id="rso"]/div[{i}]/div/div/div[1]/div/a/h3').click()
-<<<<<<< HEAD
-elements = driver.find_elements(By.PARTIAL_LINK_TEXT,'school_distric')
-=======
 elements = driver.find_elements(By.PARTIAL_LINK_TEXT,'school districts')
->>>>>>> 9c812d91250c2af33d5bd0580158287b89e94f5d
 time.sleep(0.5)
 
 for element in elements:
     try:
-<<<<<<< HEAD
-        with open("sites.txt", "a") as arquivo:
-            arquivo.write(element.get_attribute("href") + "\n")
-        element.click()
-=======
         print(element.get_attribute('title') + '\n')
         #with open("escolas.txt", "a") as arquivo:
          #   arquivo.write(element.get_attribute('title') + '\n')
-
->>>>>>> 9c812d91250c2af33d5bd0580158287b89e94f5d
         time.sleep(3)
     except ex.ElementNotInteractableException:
         print("Elemento não interagivel!")
