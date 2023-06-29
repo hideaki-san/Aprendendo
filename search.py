9c812d91250c2af33d5bd0580158287b89e94f5d
hideaki <lucashideaki07@gmail.com>
2023-06-29 12:19:56 -0300

    listagem os estados do wiki

--- search.py
+++ search.py
@@ -6,25 +6,19 @@ import time
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(0.5)
-driver.get("https://www.google.com/")
-m = driver.find_element(By.NAME,"q")
-m.send_keys("US schools list")
-time.sleep(0.2)
-m.send_keys(Keys.ENTER)
+driver.get("https://en.wikipedia.org/wiki/Lists_of_schools_in_the_United_States")
 time.sleep(0.5)
 # driver.find_element(By.XPATH,f'//*[@id="rso"]/div[{i}]/div/div/div[1]/div/a/h3').click()
-elements = driver.find_elements(By.PARTIAL_LINK_TEXT,'school')
+elements = driver.find_elements(By.PARTIAL_LINK_TEXT,'school districts')
 time.sleep(0.5)
 
 for element in elements:
     try:
-        with open("sites.txt", "a") as arquivo:
-            arquivo.write(element.get_attribute("href"))
-        element.click()
+        print(element.get_attribute('title') + '\n')
+        #with open("escolas.txt", "a") as arquivo:
+         #   arquivo.write(element.get_attribute('title') + '\n')
+
         time.sleep(3)
-        if element:
-            driver.back()
-        time.sleep(1)
     except ex.ElementNotInteractableException:
         print("Elemento não interagivel!")
     except ex.StaleElementReferenceException:
