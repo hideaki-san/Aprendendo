06e48bd031195fdb889fe7b85dc28d995c48768f
hideaki <lucashideaki07@gmail.com>
2023-06-30 11:56:19 -0300

    listagem das cidades de todos os distritos

--- searchDistrioSelect.py
+++ searchDistrioSelect.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-import selenium.common.exceptions as ex
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
@@ -9,9 +9,24 @@ driver.implicitly_wait(0.5)
 driver.get("https://nces.ed.gov/globallocator/index.asp?search=1&State=AL&city=Abbeville&zipcode=&miles=&itemname=&sortby=state&School=1&PrivSchool=1&College=1&CS=DE4547D0")
 time.sleep(0.5)
 
-element = Select(driver.find_element(By.XPATH, '//*[@id="state"]'))
-option_list = element.options
+distritos = Select(driver.find_element(By.XPATH, '//*[@id="state"]'))
+tela_cidades = driver.find_element(By.PARTIAL_LINK_TEXT,'Browse')
+todas_opcoes = distritos.options
 
-for list_position in range(7):
-    element.select_by_index(list_position)
-    time.sleep(3)
+for distrito in todas_opcoes:
+    opcao_atual = distrito.get_attribute('value')
+    
+    if opcao_atual and opcao_atual != 'OTHER':
+        distrito.click()
+        time.sleep(1)
+        tela_cidades.click()
+        time.sleep(1)
+        janela_atual = driver.window_handles
+        driver.switch_to.window(janela_atual[1])
+        time.sleep(1)
+        nome_cideades = driver.find_elements(By.TAG_NAME, 'li')
+    
+        for n in nome_cideades:
+            print(n.text)
+        
+        driver.switch_to.window(janela_atual[0])
