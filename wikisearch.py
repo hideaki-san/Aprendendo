0f6ec3bd6e8945fd72a0ed47c87de6766049bc76
ViniAman01 <vinisaman@gmail.com>
2023-06-30 09:51:00 -0400

    Edit wikisearch

--- wikisearch.py
+++ wikisearch.py
@@ -8,15 +8,18 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(0.5)
 driver.get("https://en.wikipedia.org/wiki/Lists_of_schools_in_the_United_States")
 time.sleep(1)
-element = driver.find_element(By.PARTIAL_LINK_TEXT,'school district')
+elements = driver.find_elements(By.PARTIAL_LINK_TEXT,'district')
 time.sleep(1)
-try:
-    time.sleep(1)
-    element.click()
-    time.sleep(1)
-except ex.ElementNotInteractableException:
-    print("Elemento não interagivel!")
-except ex.StaleElementReferenceException:
-    print("Elemento Obsoleto!")
-except ex.ElementClickInterceptedException:
-    print("Click interceptado!")
+for element in elements:
+    try:
+        time.sleep(1)
+        element.click()
+        time.sleep(1)
+        driver.back()
+        time.sleep(1)
+    except ex.ElementNotInteractableException:
+        print("Elemento não interagivel!")
+    except ex.StaleElementReferenceException:
+        print("Elemento Obsoleto!")
+    except ex.ElementClickInterceptedException:
+        print("Click interceptado!")
