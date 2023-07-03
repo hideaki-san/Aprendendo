0487bc51089650e39457e66c9608894af423fddf
hideaki <lucashideaki07@gmail.com>
2023-07-03 08:42:26 -0300

    exclusao de codigos nao utilizados

--- livros.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-abrir = webdriver.Chrome()
-
-abrir.get("https://www.alura.com.br/artigos/linux-compactando-e-descompactando-arquivos-com-o-tar?gclid=Cj0KCQjw1_SkBhDwARIsANbGpFvpNvTd6rmtfA-Ifid3e3X6W-ugKvjSN6KvyCp0bv8_U2zT-5-xnGwaAsDqEALw_wcB")
