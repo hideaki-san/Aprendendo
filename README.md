dea11ed1b259c4c07a7d2b801e57d8ac0b68a8cc
hideaki <lucashideaki07@gmail.com>
2023-07-04 11:34:10 -0300

    testes finalizados

---
+++ README.md
@@ -0,0 +1,36 @@
+# Rodando Aplicação
+
+## A Aplicação irá listar em um arquivo csv os nomes de todas as escolas dos Estados Unidos com o seu endereço, tipo(privada ou pública), telefone e PK.
+
+**1.** Versão utilizada do python: 3.9.17
+
+    - 1.1 Para fins de testes crie um ambiente virtual para rodar a aplicação:
+
+        ```python -m venv caminho para a pasta com o nome referente do ambiente```
+    
+    -1.2 Iniciando o ambiente:
+
+        ```source nome_da_pasta/bin/activate```
+
+    -1.3 Encerrando sessão do ambiente virtual
+        
+        ```deactivate```
+
+    - 1.4 Documentação:
+        
+        <https://docs.python.org/pt-br/3/library/venv.html>
+
+
+**2.** Instalação das bibliotecas pendentes:
+
+    ```pip install -r requirements.txt```
+
+**3.** Webdriver necessário para inicialização do navegar pelo selenium:
+
+    > Link com a documentação dos drivers para os navegadores
+    
+    <https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location/>
+
+**4.** Realizando a execução da aplicação:
+
+    ```python main.py```
--- classes/school.py
+++ classes/school.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from classes.window import WindowOperations
 from classes.city import CityOperations
 from classes.state import StateOperations
+from selenium.common.exceptions import NoSuchElementException
+
 import csv
 
 class SchoolOperations:
@@ -33,40 +35,52 @@ class SchoolOperations:
             if not is_checked_private:
                 checkbox_private.click()
 
-    def adjust_description_line(self,csv_file,description,type_school):
+    def adjust_description_line(self,csv_file,desc,type_school):
         spam_writer = csv.writer(csv_file, dialect='excel')
-        description_list = description.text.splitlines() 
-        description_list = description_list[:-1]
-        print(f'Full desc: {description_list}')
+        description_list = desc.text.splitlines()
         begin = 0
         for index in range(len(description_list)):
             end = index+1
             if end % 3 == 0:
-                if end+1 > description_list[begin:end+1]:
-
                 description_line = description_list[begin:end]
-                print(f'Line desc: {description_line}')
                 description_line.append(type_school)
                 spam_writer.writerow(description_line)
                 begin = end
 
-    def set_school_description(self,csv_file,number_school): #School class
-        public_schools_descriptions = self.driver.find_elements(By.ID,'hiddenitems_school') #Todas as escolas publicas vão pra essa variavel necessario novo tratamento
-        private_schools_descriptions = self.driver.find_elements(By.ID,'hiddenitems_privschool') #O mesmo para as privadas
+    def set_school_description(self,csv_file): #School class
+        public_desc = []
+        private_desc = []
 
+        try:
+            public_schools_descriptions = self.driver.find_element(By.ID,'hiddenitems_school')      
+            for d in public_schools_descriptions.find_elements(By.CLASS_NAME, 'InstDesc'):
+                if d.get_attribute('align') != 'center':
+                    public_desc.append(d)
+        except NoSuchElementException:
+            pass
+        
+        try:
+            private_schools_descriptions = self.driver.find_element(By.ID,'hiddenitems_privschool') 
+            for d in private_schools_descriptions.find_elements(By.CLASS_NAME,  'InstDesc'):
+                if d.get_attribute('align') != 'center':
+                    private_desc.append(d)
+        except NoSuchElementException:
+            pass
+        
+        if public_desc != []:
+            for description in public_desc:
+                self.adjust_description_line(csv_file,description,'Public')
 
-        for description in public_schools_descriptions:
-             self.adjust_description_line(csv_file,description,'Public')
+        if private_desc != []:
+            for description in private_desc:
+                self.adjust_description_line(csv_file,description,'Private')
 
-        for description in private_schools_descriptions:
-             self.adjust_description_line(csv_file,description,'Private')
-
-    def search_school(self,number_school,type_school):
+    def search_school(self):
         window = WindowOperations(self.driver)
         city = CityOperations(self.driver)
         state = StateOperations('select',self.driver)
 
-        self.set_type_of_school(type_school)
+        self.set_type_of_school('Public Private')
         states_options = state.return_select_options()
 
         csv_file = open('US_schools.csv', 'w', newline='')
@@ -74,6 +88,7 @@ class SchoolOperations:
         spam_writer.writerow(["Name","Adress","Phone","Type"])
 
         index = 0
+        result = 0
         for index in range(len(states_options)):
             states_options[index].click()
             window.open_citys_window()
@@ -83,7 +98,7 @@ class SchoolOperations:
             window.switch_window(0)
             for city_name in citys_names:
                 city.put_city_in_input_box(city_name)
-                self.set_school_description(csv_file,number_school)
+                self.set_school_description(csv_file)
 
             states_options = state.return_select_options()
             index += 1
